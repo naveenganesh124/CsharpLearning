cebf12c883263e8b8e712006e607a66b9c14f441
Naveen <n.d.selvaraj@accenture.com>
2025-03-14 10:46:29 +0530

    Console logs learning

---
+++ console.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+
+namespace learn1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("welcome to C#");
+            Console.WriteLine("    _");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/    |");
+            Console.WriteLine("_____");
+            Console.ReadLine();
+        }
+
+    }
+}
