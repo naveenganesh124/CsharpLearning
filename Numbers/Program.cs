2896ce510b510f5034ee2868f95c12a680af7a5c
Naveen <n.d.selvaraj@accenture.com>
2025-03-14 13:31:51 +0530

    Numbers learning

---
+++ Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿class Numbers
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(5);
+        Console.WriteLine(5 + 3);
+        Console.WriteLine(3 - 2);
+        Console.WriteLine(3 * 2);
+        Console.WriteLine(5 % 2);
+        Console.WriteLine(5 / 2);
+        Console.WriteLine(5 / 2.0);
+        Console.WriteLine(1.0 + 2);
+        Console.WriteLine(1.0 + 2.0);
+        int num = 5;
+        num++;
+        Console.WriteLine(num);
+
+        //MAth operation
+
+        Console.WriteLine(Math.Min(12,24));
+        Console.WriteLine(Math.Max(12, 24));
+        Console.WriteLine(Math.Abs(3.45657));
+        Console.WriteLine(Math.Pow(3,3));
+        Console.WriteLine(Math.Sqrt(37));
+
+    }
+}
