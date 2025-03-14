ccdd2087d9a4a6f35067ab846f86d2c5facf403a
Naveen <n.d.selvaraj@accenture.com>
2025-03-14 11:40:09 +0530

    Variables learning

---
+++ Console/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace learn1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string characterName = "Naveen";
+            int characterAge;
+            characterAge = 45;
+            Console.WriteLine("There oen was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Vijay";  //we can reassign the variables after sometimes.
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("but didn't like the age " + characterAge);
+
+            Console.ReadLine();
+        }
+
+    }
+}
