12b6112caafda954189b3d0cdd758099b1808d12
Naveen <n.d.selvaraj@accenture.com>
2025-03-14 13:47:34 +0530

    Getting user input

---
+++ UserInput/Program.cs
@@ -0,0 +1,12 @@
+﻿class UserInput
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your Name :");
+        string name = Console.ReadLine();
+        Console.Write("enter your Age :");
+        string age = Console.ReadLine();
+        Console.WriteLine("Hi " + name + "and " + age +" years old");
+
+    }
+}
