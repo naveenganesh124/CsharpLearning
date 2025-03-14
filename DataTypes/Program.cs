a90ec0f4384fc065c8e57f840a176d616ee61b41
Naveen <n.d.selvaraj@accenture.com>
2025-03-14 12:14:29 +0530

    Data types learning

---
+++ DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿class DataTypes
+{
+    static void Main(string[] args)
+    {
+        string userName = "Naveen"; //strings
+        char grade = 'A';  // it allow only on chaacter
+        int age = 30;
+        double gpa = 3.1;  // for decimal values
+        bool isMale = true;
+
+
+        //Without storing, we can use the variables directly into consoles also.
+        Console.WriteLine(30);
+        Console.WriteLine("test");
+        Console.WriteLine(false);
+        Console.WriteLine(2.3);
+
+        Console.WriteLine(userName);
+        Console.WriteLine(grade);
+        Console.WriteLine(age);
+        Console.WriteLine(gpa);
+        Console.WriteLine(isMale);
+
+        Console.ReadLine();
+
+
+    }
+}
